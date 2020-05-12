1778e0c9204233e0e50ff9d70ae13ec4069415bc
WPG2000 <willliampg@gmail.com>
2020-05-12 15:24:18 -0300

    Add files via upload

---
+++ engine/js/formTools/form_only_numbers_field.js
@@ -0,0 +1,10 @@
+setTimeout(function(){
+	
+	$(".numbers").keypress(function (e){
+	  var charCode = (e.which) ? e.which : e.keyCode;
+	  if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+		return false;
+	  }
+	});//end keyPressNumbers
+
+}, 1000);
